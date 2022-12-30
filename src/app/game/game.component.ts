2f8e9b37a72bc9d08e20faf930389b8b44331b9e
bkamagwork <201811374@gordoncollege.edu.ph>
2022-12-30 17:34:39 +0800

    fixed function and add restriction in using inspect element

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { compileDeclareDirectiveFromMetadata } from '@angular/compiler';
+import { Component, HostListener, OnInit } from '@angular/core';
 import * as _ from 'lodash';
 
 @Component({
@@ -6,29 +7,68 @@ import * as _ from 'lodash';
   templateUrl: './game.component.html',
   styleUrls: ['./game.component.css'],
 })
+
+
 export class GameComponent implements OnInit {
   imageUrls1 = ['/assets/word1/image1.jpg', '/assets/word1/image2.jpg'];
   imageUrls2 = ['/assets/word1/image3.jpg', '/assets/word1/image5.jpg'];
 
-  currentWord: any;
+
+  @HostListener('contextmenu', ['$event'])
+onRightClick(event: any) {
+  event.preventDefault();
+}
+
+@HostListener('document:keydown', ['$event'])
+handleKeyboardEvent(e: KeyboardEvent) {
+    if (e.key === 'F12') {
+        return false;
+    }
+    if (e.ctrlKey && e.shiftKey && e.key === "I") {
+        return false;
+    }
+    if (e.ctrlKey && e.shiftKey && e.key === "C") {
+        return false;
+    }
+    if (e.ctrlKey && e.shiftKey && e.key === "J") {
+        return false;
+    }
+    if (e.ctrlKey && e.key == "U") {
+        return false;
+    }
+    return true;
+}
+
+
+  currentWord= "";
   userGuess = '';
   message: string = '';
 
-  constructor() {}
+  numberOfTries = 0;
+
+  constructor() {
+    document.addEventListener('contextmenu', function(e) {
+      e.preventDefault();
+    });
+  }
 
   ngOnInit(): void {}
 
-  checkAnswer($event: KeyboardEvent) {
-    if ($event.key === 'Enter') {
-      this.checkGuess();
-    }
-  }
+  // checkAnswer($event: KeyboardEvent) {
+  //   if ($event.key === 'Enter') {
+  //     this.checkGuess();
+      
+  //   }
+  // }
 
   checkGuess() {
-    if (this.userGuess.toLowerCase() === this.currentWord.word.toLowerCase()) {
-      this.message = 'Correct!';
+    if (this.userGuess.toLowerCase() === this.currentWord.toLowerCase()) {
+      // this.message = 'Correct!';
+      console.log('Correct');
     } else {
-      this.message = 'Incorrect. Try again.';
+      // this.message = 'Incorrect. Try again.';
+      console.log('Incorrect. Try again.');
     }
+
   }
 }
